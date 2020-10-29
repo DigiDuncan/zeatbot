536c2d50f2375b441f5cc845e75b07174e46c93e
DigiDuncan <digiduncan@gmail.com>
2020-10-28 23:12:51 -0400

    Finish conf.

--- zeatbot/conf.py
+++ zeatbot/conf.py
@@ -1,21 +1,27 @@
 import importlib.resources as pkg_resources
+import logging
+from pathlib import Path
 
 import toml
 
 import zeatbot.data
 from zeatbot.lib import utils
 
+logger = logging.getLogger("zeatbot")
+
 confpath = pkg_resources.read_text(zeatbot.data, "settings.ini")
+oauthpath = Path(__file__).resolve().parent / "authtoken.txt"
 
 prefix = None
 botname = None
 streamername = None
 displayname = None
 timedmessagedelay = None
+oauth = None
 
 
 def load():
-    global prefix, botname, streamername, displayname, timedmessagedelay
+    global prefix, botname, streamername, displayname, timedmessagedelay, oauth
     configDict = toml.load(confpath)
 
     if utils.hasPath(configDict, "settings.prefix"):
@@ -28,6 +34,11 @@ def load():
         displayname = utils.getPath(configDict, "settings.displayname")
     if utils.hasPath(configDict, "settings.timedmessagedelay"):
         timedmessagedelay = utils.getPath(configDict, "settings.timedmessagedelay")
+    try:
+        with open(oauthpath) as f:
+            oauth = f.readlines()
+    except FileNotFoundError:
+        logger.error("OAuth token not found! Cannot log in.")
 
 
 # Force load when the module loads.
--- zeatbot/main.py
+++ zeatbot/main.py
@@ -4,6 +4,9 @@
 
 import logging
 from digiformatter import logger as digilogger
+from zeatbot import conf
+from zeatbot.lib.irc import IRC
+
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)

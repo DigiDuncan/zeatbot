1170f95c7c1589773e53249ec63c5defe8e1e6af
DigiDuncan <digiduncan@gmail.com>
2020-10-30 17:02:51 -0400

    Added logging for weather.

--- zeatbot/modules/baked.py
+++ zeatbot/modules/baked.py
@@ -1,12 +1,16 @@
 # import json
+import logging
 import requests
 
 from zeatbot import conf
+
+logger = logging.getLogger("zeatbot")
 
 
 def on_message(irc, message):
     if message.content.starts_with("{conf.prefix}weather"):
         city = message.content.remove_prefix("{conf.prefix}weather").strip()
+        logger.info(f"{message.nick} requested weather info {city}.")
         weather(irc, city)
 
 
@@ -16,6 +20,7 @@ def weather(irc, city):
     r_json = requests.get(url_string)
     if r_json["error"]:
         irc.sendmsg(f"{city} is not a valid location.")
+        logger.info(f"Didn't send weather info for {location}.")
         return
     loc = r_json["location"]
     location = f"{loc['name']}, {loc['region']}, {loc['country']}"
@@ -25,3 +30,4 @@ def weather(irc, city):
     temperature = r_json["current"]["temp_f"]
 
     irc.sendmsg(f"In {location}, it's {time}. It is {temperature}°F and {condition}. Humidity {humidity}.")
+    logger.info(f"Sent weather info for {location}.")

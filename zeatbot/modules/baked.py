2132d76729b9ae7ac683e3d6eaa1e4a4ac0697b7
DigiDuncan <digiduncan@gmail.com>
2020-10-31 20:27:35 -0400

    Fix weather and clean up formatting.

--- zeatbot/modules/baked.py
+++ zeatbot/modules/baked.py
@@ -2,14 +2,20 @@
 import logging
 import requests
 
+import arrow
+
 from zeatbot import conf
+from zeatbot.lib.utils import removeprefix
 
 logger = logging.getLogger("zeatbot")
 
 
 def on_message(irc, message):
-    if message.content.starts_with("{conf.prefix}weather"):
-        city = message.content.remove_prefix("{conf.prefix}weather").strip()
+    logger.info(message)
+    if message.command != "PRIVMSG":
+        return
+    if message.content.startswith(f"{conf.prefix}weather"):
+        city = removeprefix(message.content, f"{conf.prefix}weather").strip()
         logger.info(f"{message.nick} requested weather info {city}.")
         weather(irc, city)
 
@@ -17,14 +23,16 @@ def on_message(irc, message):
 # Commands
 def weather(irc, city):
     url_string = f"http://api.weatherapi.com/v1/current.json?key={conf.weather_token}&q={city}"
-    r_json = requests.get(url_string)
-    if r_json["error"]:
+    r_json = requests.get(url_string).json()
+    if "error" in r_json:
         irc.sendmsg(f"{city} is not a valid location.")
         logger.info(f"Didn't send weather info for {city}.")
         return
     loc = r_json["location"]
     location = f"{loc['name']}, {loc['region']}, {loc['country']}"
-    time = loc['localtime']
+    temptime = loc['localtime']
+    timearrow = arrow.get(temptime, "YYYY-MM-DD H:mm")
+    time = timearrow.format("DD MMM YYYY") + ", " + timearrow.format("hh:mm") + " (" + timearrow.format("ZZZ") + " time)"
     condition = r_json["current"]["condition"]["text"]
     humidity = f"{r_json['current']['humidity']}%"
     temperature = r_json["current"]["temp_f"]
